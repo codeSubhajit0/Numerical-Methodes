b2a5a61b27a61f8dd0495a598fdbb50e123dd80a
Subhajit Karmakar <subhajitk731@gmail.com>
2023-04-16 12:11:36 -0800

    new commit

---
+++ whatPrintF_FunctionReturn.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void  main(){
+    int a = 0;
+    a = printf("What printf() function return? \n");
+    printf("printf() function return the length of the printed string , which is in this case %d \n", a);
+}
